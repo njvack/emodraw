299f9d178cf9d1c592c74f3a0e7febcca50a1a61
Nate Vack <njvack@wisc.edu>
2014-07-25 15:11:48 -0500

    Save image after saving associated model

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,9 +1,12 @@
 class Avatar < ActiveRecord::Base
   belongs_to :experiment
   has_one :avatar_image, :as => :imageable
+  attr_writer :uploaded_file
 
-  def uploaded_file=(file)
+  after_save :save_image_file!
+
+  def save_image_file!
     self.avatar_image.delete if self.avatar_image
-    self.create_avatar_image(:content_type => file.content_type, :data => file.read)
+    self.create_avatar_image(:content_type => @uploaded_file.content_type, :data => @uploaded_file.read)
   end
 end
