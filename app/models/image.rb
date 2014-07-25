85ec72bb71ac080b899343d3ce76029ba58469ff
Nate Vack <njvack@wisc.edu>
2014-07-25 15:12:37 -0500

    Set a width and height of images as they’re saved
    We’ll want that to make page layouts happen fast.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,15 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
+
+  before_save :set_dimensions, :if => :has_data?
+
+  def set_dimensions
+    img = MiniMagick::Image.read(self.data)
+    self.height = img[:height]
+    self.width = img[:width]
+  end
+
+  def has_data?
+    self.data.present?
+  end
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    extend ActionDispatch::TestProcess
+  end
+
+  test "saving sets height and width" do
+    af = fixture_file_upload('1x1.gif', 'image/gif')
+    img = Image.create(
+      :type => 'Image',
+      :imageable_type => 'TestImageable',
+      :imageable_id => 0,
+      :content_type => af.content_type,
+      :data => af.read()
+    )
+    assert_equal img.height, 1
+    assert_equal img.width, 1
+  end
 end
